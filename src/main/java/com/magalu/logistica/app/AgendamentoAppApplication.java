740c9daf64eca0e3734f5cf7a6b5a7afe86fb726
fernandoseparovic <31331254+fernandoseparovic@users.noreply.github.com>
2021-06-17 21:31:54 -0300

    Estrutura inicial projeto spring-boot

---
+++ src/main/java/com/magalu/logistica/app/AgendamentoAppApplication.java
@@ -0,0 +1,13 @@
+package com.magalu.logistica.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoAppApplication.class, args);
+	}
+
+}
