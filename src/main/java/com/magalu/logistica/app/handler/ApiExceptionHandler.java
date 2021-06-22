b5cbba74fc48dba6917fe99ed0be4a0a9b95b7a4
fernandoseparovic <31331254+fernandoseparovic@users.noreply.github.com>
2021-06-21 23:14:31 -0300

    Tratamento de exception para erros de negocio

--- src/main/java/com/magalu/logistica/app/handler/ApiExceptionHandler.java
+++ src/main/java/com/magalu/logistica/app/handler/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.magalu.logistica.app.handler;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -9,9 +10,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.magalu.logistica.app.agendamento.api.exception.BusinessException;
 
 /**
  * Intercepta as exceptions e realiza os devidos tratamentos
@@ -38,5 +42,16 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 											 .collect(Collectors.toMap(error -> ((FieldError) error).getField(), 
 													 				   ObjectError::getDefaultMessage));
 		return new ResponseEntity<>(errors, status);
+	}
+
+
+	@ExceptionHandler({ BusinessException.class })
+	public final ResponseEntity<Object> handleBusinessException(final BusinessException ex, 
+																final WebRequest request) {
+		
+		final Map<String, String> errors = new HashMap<>();
+		errors.put("Erro negocio: ", ex.getMessage());
+		
+		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
 	}
 }
